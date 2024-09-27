e08810f44a51a4ca5253c48f5032397b3b514dd6
Djaysambo <vicentesambo3@gmail.com>
2024-09-28 00:44:36 +0200

    updat: Home

--- src/app/components/projects/projects.tsx
+++ src/app/components/projects/projects.tsx
@@ -1,42 +1,52 @@
+import React from 'react';
 
-import React, { Component } from 'react'
-
-export const Projects =()=> {
-  
-    return (
-      <>
-      <div className="carousel carousel-end rounded-box translate-x-1 animate-pulse ">
-  <div className="carousel-item">
-    <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" alt="Drink" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-      alt="Drink" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-      alt="Drink" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-      alt="Drink" />
-  </div>
-  <div className="carousel-item">
-    <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" alt="Drink" />
-  </div>
-  <div className="carousel-item">
-    <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" alt="Drink" />
-  </div>
-  <div className="carousel-item">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-      alt="Drink" />
-  </div>
-</div> 
-      </>
-    )
-  };
-
+export const Projects: React.FC = () => {
+  return (
+    <>
+      <div className="carousel carousel-end rounded-box translate-x-1 animate-pulse">
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+            alt="Drink"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+            alt="Drink"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+            alt="Drink"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+            alt="Drink"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+            alt="Drink"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+            alt="Drink"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+            alt="Drink"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,25 @@ import Image from "next/image";
 import { Footer } from "./components/footer/footer";
 import { Projects } from "./components/projects/projects";
 import { Header } from "./components/header/header";
+import { HomeInfo } from "./components/home/home";
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <>
-        <div className="">
-      <div className="static top-0">
-      <Header/>
+      <div className="">
+        <div className="static top-0">
+          <Header />
+        </div>
+        <HomeInfo />
+        <div className="space-y-4">
+          <Projects />
+          <Projects />
+        </div>
+        <Footer />
       </div>
-      <div className="space-y-4">
-      <Projects/>
-      <Projects/>
-      </div>
-     
-    </div>
-     <Footer/>
-
+      
     </>
   );
-}
+};
+
+export default Home;
